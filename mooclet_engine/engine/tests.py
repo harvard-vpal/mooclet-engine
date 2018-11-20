d0be12292c2366b02442e048278701fbc42f9a40
Hammad Shaikh <hammy.shaikh@mail.utoronto.ca>
2018-11-20 18:27:22 -0500

    Comment test_thompson_sampling_contextual function

--- mooclet_engine/engine/tests.py
+++ mooclet_engine/engine/tests.py
@@ -8,23 +8,40 @@ from policies import thompson_sampling_contextual
 class PolicyParams():
     parameters = {}
 
+# Function used for testing the linear contextual bandit
 def test_thompson_sampling_contextual():
+    
+    # Number of regression parameters
     dim = 6
     vars = None
     contexts = {}
     policy_params = PolicyParams()
-    policy_params.parameters['regression_formula'] = 'y ~ x0 + x1 + matching_1 + matching_2 + charity'
-    policy_params.parameters['action_space'] = {'matching_1': [0,1],'matching_2':[0,1],'charity':[0,1,2]}
-    policy_params.parameters['include_intercept'] = True
-    policy_params.parameters['contextual_variables'] = {'x0': 2.5,'x1':-0.7}
+    
+    # Specify regression model
+    # policy_params.parameters['regression_formula'] = 'y ~ x0 + x1 + matching_1 + matching_2 + charity'
+    
+    policy_params.parameters['regression_formula'] = 'y ~ Age + charity_2 + charity_3 + matching_2 + matching_3'
 
+    # Specify action variables
+    policy_params.parameters['action_space'] = {'charity_2': [0,1],'charity_3':[0,1],'matching_2':[0,1], 'matching_3':[0,1]}
+    # Specify intercept
+    policy_params.parameters['include_intercept'] = True
+    # Specify context
+    policy_params.parameters['contextual_variables'] = {'Age': 19}
+
+    ## Specify normal-inverse-gamma parameters 
+    # Mean of coefficients 
     policy_params.parameters['coef_mean'] = np.zeros(dim)
+    # Covariance matrix of coefficients
     policy_params.parameters['coef_cov'] = np.identity(dim)
+    # Shape and scale parameters
     policy_params.parameters['variance_a'] = 2
     policy_params.parameters['variance_b'] = 2
 
+    # Store normal inverse gamma parameters
     contexts['policy_parameters'] = policy_params
-
+    
+    # Run thompson sampling function
     thompson_sampling_contextual(vars, contexts)
 
 test_thompson_sampling_contextual()
